7a09143d674d64993886262368a8d5ffa67190a9
mmizzle9 <mmizzle9@gmail.com>
2015-08-02 21:01:53 -0400

    Fixed broken ass line in stupid Makefile

--- djinni/example/android/app/jni/Application.mk
+++ djinni/example/android/app/jni/Application.mk
@@ -1,10 +1,9 @@
 # Android makefile for textsort shared lib, jni wrapper around libtextsort C API
-
 APP_ABI := all
 APP_OPTIM := release
-APP_PLATFORM := android-8
+APP_PLATFORM := android-16
 # GCC 4.8 Toolchain - requires NDK r9
-NDK_TOOLCHAIN_VERSION = 4.8
+NDK_TOOLCHAIN_VERSION = 4.9
 # GNU libc++ is the only Android STL which supports C++11 features
 APP_STL := gnustl_static
 APP_BUILD_SCRIPT := jni/Android.mk
